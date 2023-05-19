b300e1cb7b910f6b59dbb4e8469d001d12569b7c
sergio <learningbyplaying>
2023-05-19 21:36:31 +0200

    ok

--- scenarios/helloworld/app/producer.py
+++ scenarios/helloworld/app/producer.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from confluent_kafka import Producer
 from confluent_kafka.admin import AdminClient, NewTopic
+from pydantic import BaseModel
 
 app = FastAPI()
+
+producer_config = {
+    'bootstrap.servers': 'kafka:9092',  # Update with your Kafka broker's address
+    'client.id': 'fastapi-producer'
+}
+
+topic = 'my_topic'
 
 async def kafka_startup():
     # Kafka AdminClient Configuration
@@ -12,7 +20,7 @@ async def kafka_startup():
     # Create AdminClient
     admin_client = AdminClient(admin_config)
     # Define the topic to be created
-    topic_name = 'my_topic'  # Update with your desired topic name
+    topic_name = topic
     num_partitions = 3  # Specify the number of partitions for the topic
     replication_factor = 1  # Specify the replication factor for the topic
 
@@ -23,24 +31,16 @@ async def kafka_startup():
     print(admin_client.list_topics().topics)
 
 
-@app.get("/")
-async def read_root():
+class Message(BaseModel):
+    text: str
+
+@app.post("/")
+async def read_root(message: Message):
 
     await kafka_startup()
-    producer_config = {
-        'bootstrap.servers': 'kafka:9092',  # Update with your Kafka broker's address
-        'client.id': 'fastapi-producer'
-    }
 
-        # Create Kafka Producer
     producer = Producer(producer_config)
-
-    # Produce a message
-    topic = 'my_topic'  # Update with your desired Kafka topic
-    message = 'Hello Kafka!'
-    producer.produce(topic, value=message.encode('utf-8'))
-
-    # Flush and wait for delivery reports
+    producer.produce(topic, value=message.text.encode('utf-8'))
     producer.flush()
 
     return {"Kafka": "Producer"}
