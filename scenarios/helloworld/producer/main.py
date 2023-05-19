081245d98a743cfdd62337caac9a2a2d96eb77c3
sergio <learningbyplaying>
2023-05-19 19:59:15 +0200

    ok

--- scenarios/helloworld/producer/main.py
+++ scenarios/helloworld/producer/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from confluent_kafka import Producer
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
- return {"Kafka": "Producer2"}
+
+    producer_config = {
+        'bootstrap.servers': 'kafka:9092',  # Update with your Kafka broker's address
+        'client.id': 'fastapi-producer'
+    }
+
+        # Create Kafka Producer
+    producer = Producer(producer_config)
+
+    # Produce a message
+    topic = 'my_topic'  # Update with your desired Kafka topic
+    message = 'Hello Kafka!'
+    producer.produce(topic, value=message.encode('utf-8'))
+
+    # Flush and wait for delivery reports
+    producer.flush()
+    return {"Kafka": "Producer2"}
