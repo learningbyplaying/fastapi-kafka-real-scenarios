bf0d684b65c955172c6bf61b73d7acc066c14486
sergio <learningbyplaying>
2023-05-19 21:01:39 +0200

    ok

--- scenarios/helloworld/producer/main.py
+++ scenarios/helloworld/producer/main.py
@@ -1,11 +1,32 @@
 from fastapi import FastAPI
 from confluent_kafka import Producer
+from confluent_kafka.admin import AdminClient, NewTopic
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
+async def kafka_startup():
+    # Kafka AdminClient Configuration
+    admin_config = {
+        'bootstrap.servers': 'kafka:9092'  # Update with your Kafka broker's address
+    }
+    # Create AdminClient
+    admin_client = AdminClient(admin_config)
+    # Define the topic to be created
+    topic_name = 'my_topic'  # Update with your desired topic name
+    num_partitions = 3  # Specify the number of partitions for the topic
+    replication_factor = 1  # Specify the replication factor for the topic
 
+    # Create a NewTopic object with the topic configuration
+    new_topic = NewTopic(topic_name, num_partitions, replication_factor)
+    # Create the topic using the AdminClient
+    admin_client.create_topics([new_topic])
+    print(admin_client.list_topics().topics)
+
+
+@app.get("/")
+async def read_root():
+
+    await kafka_startup()
     producer_config = {
         'bootstrap.servers': 'kafka:9092',  # Update with your Kafka broker's address
         'client.id': 'fastapi-producer'
--- scenarios/helloworld/schema-setup/main.py
+++ scenarios/helloworld/schema-setup/main.py
@@ -1,29 +1,41 @@
 from confluent_kafka.admin import AdminClient, NewTopic
-import pprint
+import time
 
-# Kafka AdminClient Configuration
-admin_config = {
-    'bootstrap.servers': 'kafka:9092'  # Update with your Kafka broker's address
-}
+def startup():
+    # Kafka AdminClient Configuration
+    admin_config = {
+        'bootstrap.servers': 'kafka:9092'  # Update with your Kafka broker's address
+    }
+    # Create AdminClient
+    #admin_client = AdminClient(admin_config)
 
-# Create AdminClient
-admin_client = AdminClient(admin_config)
+    # Define the topic to be created
+    topic_name = 'my_topic'  # Update with your desired topic name
+    num_partitions = 3  # Specify the number of partitions for the topic
+    replication_factor = 1  # Specify the replication factor for the topic
 
-# Define the topic to be created
-topic_name = 'my_topic'  # Update with your desired topic name
-num_partitions = 3  # Specify the number of partitions for the topic
-replication_factor = 1  # Specify the replication factor for the topic
+    # Create a NewTopic object with the topic configuration
+    #new_topic = NewTopic(topic_name, num_partitions, replication_factor)
 
-# Create a NewTopic object with the topic configuration
-new_topic = NewTopic(topic_name, num_partitions, replication_factor)
+    # Create the topic using the AdminClient
+    #r = admin_client.create_topics([new_topic])
+    #print(new_topic, admin_client, r)
+    #print(admin_client.list_topics().topics)
+
+    print('TESTING a LOOP')
 
 
-# Create the topic using the AdminClient
-r = admin_client.create_topics([new_topic])
+if __name__ == "__main__":
 
-#print(new_topic, admin_client, r)
+    while True:
 
-print(admin_client.list_topics().topics)
+        try:
+            print("Starting kafka topis schema...")
 
-# Close the AdminClient
-#admin_client.close()
+            startup()
+
+
+        except Exception as e:
+            print(f"Exception occurred: {e}")
+
+        time.sleep(1)  # Sleep for 1 second
