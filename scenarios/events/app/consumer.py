ca3589a2588146ecdb298ec613d7d0c7f5bf53af
sergio <learningbyplaying>
2023-05-21 00:28:17 +0200

    ok

--- scenarios/events/app/consumer.py
+++ scenarios/events/app/consumer.py
@@ -13,9 +13,9 @@ conf = {
 def consume_messages(channel):
 
     base_path = '/app/channels/{}'.format(channel)
-    avro_schema = avro.load('{}/ecommerce_event.avsc'.format(base_path))
+    avro_schema = avro.load('{}/schema.avsc'.format(base_path))
     json_file = "{}/topic.json".format(base_path)
-    
+
     json_data = json.load(open(json_file))
     topic = json_data['topic']
     print(topic)
