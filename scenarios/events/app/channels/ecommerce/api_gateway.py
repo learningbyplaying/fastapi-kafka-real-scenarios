759ac03ae6ebe1e43b900a0034639723447a822b
sergio <learningbyplaying>
2023-05-21 00:21:44 +0200

    ok

--- scenarios/events/app/channels/ecommerce/api_gateway.py
+++ scenarios/events/app/channels/ecommerce/api_gateway.py
@@ -1,30 +1,25 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
-from confluent_kafka import Producer
 from confluent_kafka.admin import AdminClient, NewTopic
 from confluent_kafka import avro
 from confluent_kafka.avro import AvroProducer
 
 from .models import EcommerceMessage
-
-from dotenv import load_dotenv
 import os, json
-load_dotenv()
 
 #Infraestructue
+base_path = '/app/channels/ecommerce'
 schema_registry_url = 'http://schema-registry:8081'
 producer_config = {'bootstrap.servers': 'kafka:9092','client.id': 'fastapi-producer', 'schema.registry.url': schema_registry_url}
 
 #Schema
-avro_schema = avro.load('/app/channels/ecommerce/ecommerce_event.avsc')
-json_file = "/app/channels/ecommerce/topic.json"
+avro_schema = avro.load('{}/ecommerce_event.avsc'.format(base_path))
+json_file = "{}/topic.json".format(base_path)
 json_data = json.load(open(json_file))
 topic = json_data['topic']
 
-
+#endpoint
 app = FastAPI()
-
 @app.post("/events/gateway",  tags=['Ecommerce'])
 async def events(message: EcommerceMessage):
     #print(message.dict())
