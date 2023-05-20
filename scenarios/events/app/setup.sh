a0ead95ddea0d1913def507dfc7df805b28e32f3
sergio <learningbyplaying>
2023-05-20 18:35:27 +0200

    ok

--- scenarios/events/app/setup.sh
+++ scenarios/events/app/setup.sh
@@ -5,8 +5,16 @@ curl -X GET http://0.0.0.0:8081/subjects
 echo ""
 
 echo ">>POST Subjects"
+
 subject=user-value
 curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
   --data @user.avsc \
+  "http://0.0.0.0:8081/subjects/$subject/versions"
+echo ""
+
+
+subject=ecommerce-event
+curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
+  --data @channels/ecommerce/ecommerce_event.avsc \
   "http://0.0.0.0:8081/subjects/$subject/versions"
 echo ""
