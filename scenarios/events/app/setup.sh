721b9be8b8b7a7d120f42aeda9e04f0a55fffc64
sergio <learningbyplaying>
2023-05-20 18:20:17 +0200

    ok

---
+++ scenarios/events/app/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo ">>GET Subjects"
+curl -X GET http://0.0.0.0:8081/subjects
+echo ""
+
+echo ">>POST Subjects"
+subject=user-value
+curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
+  --data @user.avsc \
+  "http://0.0.0.0:8081/subjects/$subject/versions"
+echo ""
