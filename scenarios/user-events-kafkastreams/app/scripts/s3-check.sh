19f9666fed709e291ab36f9b71677ffe9637a4c5
sergio <learningbyplaying>
2023-05-30 21:47:56 +0200

    ok

--- scenarios/user-events-kafkastreams/app/scripts/s3-check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -o allexport; source /app/.credentials; set +o allexport
-
-source=/app/scripts/test.csv
-target=s3://etl-on-yaml/test.csv
-
-/usr/local/bin/aws s3 cp $source $target #--recursive
